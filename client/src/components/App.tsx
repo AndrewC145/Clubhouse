17c5a442073899d88f6644c4b4a5f2b730bcb249
Andrew Cao <andrewcao618@gmail.com>
2025-05-01 21:30:42 -0400

    Added Banner and Header

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./Header";
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto max-w-[600px]">
+        <Banner />
+      </main>
+    </>
+  );
+}
 
 export default App;
---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,9 @@
+function Banner() {
+  return (
+    <div className="mt-6 rounded-xl border-1 border-blue-300 bg-blue-400 p-3 text-center md:p-5">
+      <p className="text-white">Sign in or create an account to create posts</p>
+    </div>
+  );
+}
+
+export default Banner;
