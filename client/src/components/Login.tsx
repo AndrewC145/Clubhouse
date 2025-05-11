93cfc61174d05b2dd81e9ffdfe3a787a5ccbbd7a
Andrew Cao <andrewcao618@gmail.com>
2025-05-11 15:39:34 -0400

    Fixed Login Error Bug

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import FormTemp from "./FormTemp";
 import Input from "./Input";
@@ -8,6 +9,16 @@ function Login() {
   const [username, setUsername] = useState<string>("");
   const [success, setSuccess] = useState<string>("");
   const [errors, setErrors] = useState<string>("");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      if (isLoggedIn) {
+        navigate("/", { replace: true });
+      }
+    }, 3000);
+  }, [isLoggedIn, navigate]);
 
   const fetchUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,13 +35,16 @@ function Login() {
       if (response.status === 200) {
         const username = response.data.user.username;
         setUsername(username);
+        setIsLoggedIn(true);
         setSuccess(response.data.message);
         setErrors("");
       }
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
-      const errors = error.response.data.error;
+      const errors = error.response.data.error.message;
+      setSuccess("");
+      setIsLoggedIn(false);
       setErrors(errors);
     }
   };
@@ -40,7 +54,11 @@ function Login() {
       <Input type="text" id="username" name="username" label="Username" />
       <Input type="password" id="password" name="password" label="Password" />
       <div className="space-y-2 text-red-400">{errors && <p>{errors}</p>}</div>
-      {success && <p className="text-green-400">{success}</p>}
+      {success && (
+        <p className="text-green-400">
+          {success}! Hi there {username}. Redirecting...
+        </p>
+      )}
       <FormButton text="Login" />
     </FormTemp>
   );
